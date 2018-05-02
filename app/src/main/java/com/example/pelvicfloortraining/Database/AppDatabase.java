4d0c54c3682c2bdb0fc90f48f3393eb58569b1ef
ellor14 <36482665+ellor14@users.noreply.github.com>
2018-05-02 10:41:42 +0200

    Database

---
+++ app/src/main/java/com/example/pelvicfloortraining/Database/AppDatabase.java
@@ -0,0 +1,33 @@
+package com.example.pelvicfloortraining.Database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+/*
+
+@Database(entities = {TrainingLog.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public abstract TrainingDao trainingDao();
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "TrainingLog-database")
+                            // allow queries on the main thread.
+                            // Don't do this on a real app!
+                            .allowMainThreadQueries()
+                            .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+
+        INSTANCE = null;
+    }
+}*/
---
+++ app/src/main/java/com/example/pelvicfloortraining/Database/TrainingDao.java
@@ -0,0 +1,26 @@
+package com.example.pelvicfloortraining.Database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+/*
+
+
+@Dao
+public interface TrainingDao {
+
+    @Query("SELECT * FROM `traininglog`")
+    TrainingLog getLog();
+
+    @Update
+    int update(TrainingLog trainingLog);
+
+    @Insert
+    void insert(TrainingLog trainingLog);
+
+    @Query("SELECT COUNT(*) from `traininglog`")
+    int counttraininglog();
+
+}*/
